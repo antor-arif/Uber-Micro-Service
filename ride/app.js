31a4e46f18082e77cb5f3cf971d1558e6d771b3d
antor-arif <rahmanarifur4838@gmail.com>
2025-07-18 22:00:04 +0600

    db connect for ride

--- ride/app.js
+++ ride/app.js
@@ -2,6 +2,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const app = express();
+const connect = require('./db/db');
+connect();
 
 const cookieParser = require('cookie-parser');
 
